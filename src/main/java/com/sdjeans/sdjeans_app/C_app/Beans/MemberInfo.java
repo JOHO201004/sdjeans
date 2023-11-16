b059e8d669b72a1e35ef0f3d6a37a48fa57759cd
JOHO201004 <201004@std.hi-joho.ac.jp>
2023-11-16 16:34:34 +0900

    一時避難

---
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/Beans/MemberInfo.java
@@ -0,0 +1,7 @@
+package com.sdjeans.sdjeans_app.C_app.Beans;
+public class MemberInfo {
+    private String memberId;
+    private String memberName;
+
+    // ゲッターとセッターは省略
+}
---
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/forms/registerForm.java
@@ -0,0 +1,18 @@
+package com.sdjeans.sdjeans_app.C_app.Beans;
+
+import lombok.Data;
+
+@Data
+public class registerForm {
+    
+    //会員番号
+    private Integer memberId;
+    //パスワード
+    private String pw;
+    //確認パスワード
+    private String cfmPw;
+    //名前
+    private String name;
+    //住所
+    private String address;
+}
