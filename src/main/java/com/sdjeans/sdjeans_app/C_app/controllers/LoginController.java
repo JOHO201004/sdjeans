488c24cb7f620b872f3fb1fb3722344a35bc2a7a
unknown <201013>
2023-11-22 16:56:40 +0900

    a

--- src/main/java/com/sdjeans/sdjeans_app/C_app/controllers/LoginController.java
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/controllers/LoginController.java
@@ -11,10 +11,11 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sdjeans.sdjeans_app.C_app.Beans.loginForm;
 import com.sdjeans.sdjeans_app.C_app.Beans.memberInf;
+import com.sdjeans.sdjeans_app.C_app.Entity.Member;
+import com.sdjeans.sdjeans_app.C_app.forms.LoginForm;
 import com.sdjeans.sdjeans_app.C_app.forms.registerForm;
 import com.sdjeans.sdjeans_app.C_app.services.accountService;
 
@@ -22,43 +23,47 @@ import com.sdjeans.sdjeans_app.C_app.services.accountService;
 @Controller
 // @Slf4j
 public class LoginController {
+
     @Autowired
     accountService accountService;
 
 
     @GetMapping("/login")
-    String getLogin(Model model) {
-        model.addAttribute("loginForm", new loginForm()); // loginFormをモデルに追加する
+    public String Login(Model model) {
+        model.addAttribute("LoginForm", new LoginForm()); // loginFormをモデルに追加する
         return "c_temp/login";
     }
 
-    @PostMapping("/home")
-    public String Login(@ModelAttribute loginForm form, BindingResult result, Model model){
-        try{
-            if(form.getMemberId() == 1 && form.getPw().equals("p")){
-                System.out.println("a");
-            }
-        }catch(NullPointerException e){
-            
+    // @GetMapping("/") // ルートURL ("/") に対するGETリクエストを処理します
+    // public String redirectToIndex() {
+    //     Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // 現在のユーザーの認証情報を取得します
+    //     if (authentication != null && authentication.isAuthenticated()) { // ユーザーがログインしている場合
+    //         return "c_temp/home";  // "/index"にリダイレクトします
+    //     }
+    //     return "c_temp/login"; // ユーザーがログインしていない場合、"/login"にリダイレクトします
+    // }   
+
+    // @PostMapping("/home")
+    // public String Login(@ModelAttribute loginForm form, BindingResult result, Model model){
+    //     try{
+    //         if(form.getMemberId() == 1 && form.getPw().equals("p")){
+    //             System.out.println("a");
+    //             return "redirect:/c_temp/login";
+    //         }
+    //         if(result.hasErrors()){
+    //             model.addAttribute("registerForm", form);
+    //             return "redirect:/c_temp/login";
+    //         }
+    //     }catch(NullPointerException e){
+    //         return "c_temp/login";
+    //     }
+    //     return "redirect:/c_temp/login";
+    // }
+
+        @GetMapping("/home")
+        public String Home() {
+            return "c_temp/home";
         }
-        return "redirect:c_temp/home";
-    }
-
-     /**
-     * ログイン成功時に呼び出されるメソッド
-     * SecurityContextHolderから認証済みユーザの情報を取得しモデルへ追加する
-     * @param model リクエストスコープ上にオブジェクトを載せるためのmap
-     * @return helloページのViewName
-     */
-    @RequestMapping("/home")
-    private String init(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        //Principalからログインユーザの情報を取得
-        String userName = auth.getName();
-        model.addAttribute("userName", userName);
-        return "c_temp/home";
-
-    }
 
 
 
@@ -91,15 +96,15 @@ public class LoginController {
             return "c_temp/reMember";
         }
     }
-    @PostMapping("/regiScs")
-    public String successRegisterMember(BindingResult result,Model model){
+    // @PostMapping("/regiScs")
+    // public String successRegisterMember(BindingResult result,Model model){
         
-        try{
-        accountService.InsertMember(memInf);
-                }catch(OptimisticLockingFailureException e){
-            result.addError(new ObjectError("global", e.getMessage()));
-            return "c_temp/reMember";
-        }
-    }
+    //     try{
+    //     accountService.InsertMember(memInf);
+    //             }catch(OptimisticLockingFailureException e){
+    //         result.addError(new ObjectError("global", e.getMessage()));
+    //         return "c_temp/reMember";
+    //     }
+    // }
     
 }
--- src/main/java/com/sdjeans/sdjeans_app/C_app/mappers/accountMapper.java
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/mappers/accountMapper.java
@@ -4,10 +4,13 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.sdjeans.sdjeans_app.C_app.Beans.memberInf;
+import com.sdjeans.sdjeans_app.C_app.Entity.Member;
 
 @Mapper
 public interface accountMapper {
 
     @Insert("INSERT INTO member VALUES (#{memberId},#{pw},#{name},#{address})")
     public int InsertMember(memberInf memberInf);
+
+    Member findByMembername(String username);
 }
