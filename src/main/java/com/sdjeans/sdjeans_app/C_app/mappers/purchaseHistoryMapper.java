c58eb25e6a5adf5bd4cdb47a0eb5f88f003b3566
JOHO201004 <201004@std.hi-joho.ac.jp>
2023-11-30 11:09:27 +0900

    何とかなりましたが進みませんでした

--- src/main/java/com/sdjeans/sdjeans_app/C_app/mappers/purchaseHistoryMapper.java
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/mappers/purchaseHistoryMapper.java
@@ -2,11 +2,13 @@ package com.sdjeans.sdjeans_app.C_app.mappers;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import com.sdjeans.sdjeans_app.C_app.Beans.merchandise;
 import com.sdjeans.sdjeans_app.C_app.Beans.purchaseHistory;
+import com.sdjeans.sdjeans_app.C_app.Beans.purchaseHistoryMainKey;
 
 @Mapper
 public interface purchaseHistoryMapper {
@@ -16,4 +18,7 @@ public interface purchaseHistoryMapper {
 
     @Select("SELECT * FROM merch WHERE merch_id = #{merchId}")
     public merchandise selectMerchById(Integer merchId);
+
+    @Delete("DELETE FROM purchaseHistory WHERE member_id = #{memberId} AND merch_id #{merchId} AND deadline = #{deadline}")
+    public int deletePurchaseHistory(purchaseHistoryMainKey purchaseHistoryMainKey);
 }
