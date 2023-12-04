0aaddcf74daf1319fb112390c007e1c4fd2bd025
JOHO201004 <201004@std.hi-joho.ac.jp>
2023-12-04 16:58:56 +0900

    数量変更頑張りましtあ

--- src/main/java/com/sdjeans/sdjeans_app/C_app/controllers/purchaseHistoryController.java
+++ src/main/java/com/sdjeans/sdjeans_app/C_app/controllers/purchaseHistoryController.java
@@ -15,6 +15,7 @@ import com.sdjeans.sdjeans_app.C_app.Beans.memberId;
 import com.sdjeans.sdjeans_app.C_app.Beans.merchandise;
 import com.sdjeans.sdjeans_app.C_app.Beans.purchaseHistory;
 import com.sdjeans.sdjeans_app.C_app.Beans.purchaseHistoryMainKey;
+import com.sdjeans.sdjeans_app.C_app.Beans.purchaseHistoryQuantityUpdate;
 import com.sdjeans.sdjeans_app.C_app.Beans.purchasehistoryOfView;
 import com.sdjeans.sdjeans_app.C_app.services.purchaseHistoryService;
 
@@ -29,24 +30,37 @@ public class purchaseHistoryController {
 
     @GetMapping("/purchaseH")
     public String getPurchaseHistory(HttpSession session, Model model) {
-        // String memberId = (String)session.getAttribute("memberId");
-        String memberId = "1";
-        
+        String memberId = (String) session.getAttribute("memberId");
+        // String memberId = "0001";
+
         model.addAttribute("purchaseHistories", makePurchaseHistoryOfView(memberId));
         return "c_temp/purchaseHistory";
     }
-    
 
     @PostMapping("/purchaseH")
     public String deletePurchaseHistory(
-        // @ModelAttribute purchaseHistoryMainKey purchaseHistoryMainKey,
+            // @ModelAttribute purchaseHistoryMainKey purchaseHistoryMainKey,
             @RequestParam("memberId") String memberId,
             @RequestParam("merchId") Integer merchId,
             @RequestParam("deadline") LocalDateTime deadline,
             Model model) {
-                System.out.println("Controller reached!"); // ログの出力
+        System.out.println("Controller reached!"); // ログの出力
         // purchaseHistoryService.deletePurchaseHistory(purchaseHistoryMainKey);
-        purchaseHistoryService.deletePurchaseHistory(new purchaseHistoryMainKey(memberId,merchId,deadline));
+        purchaseHistoryService.deletePurchaseHistory(new purchaseHistoryMainKey(memberId, merchId, deadline));
+        model.addAttribute("purchaseHistories", makePurchaseHistoryOfView(memberId));
+        return "c_temp/purchaseHistory";
+    }
+
+    @PostMapping("/updateQuantity")
+    public String updatePurchaseHistory(
+            @RequestParam("updateMemberId") String memberId,
+            @RequestParam("updateMerchId") Integer merchId,
+            @RequestParam("updateDeadline") LocalDateTime deadline,
+            @RequestParam("newQuantity") Integer quantity,
+            Model model) {
+                System.out.println("ここはあっぷでーとです" + "会員" + memberId +"商品" + merchId + "期限" +deadline + "数量" + quantity);
+        purchaseHistoryService
+                .updatePurchaseHistory(new purchaseHistoryQuantityUpdate(memberId, merchId, deadline, quantity));
         model.addAttribute("purchaseHistories", makePurchaseHistoryOfView(memberId));
         return "c_temp/purchaseHistory";
     }
